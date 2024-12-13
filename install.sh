53037ac23b2965c9e5fcf739aa5f72bb318bc42a
thomassolheim <thomas.solheim@dnb.no>
2024-12-13 23:58:50 +0100

    Add brewfile. Delete .gitconfig

--- install.sh
+++ install.sh
@@ -1,20 +1,17 @@
 brew install font-fira-code-nerd-font
 
 # https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
-bash -c "$(curl --fail --show-error --silent \
-    --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+#bash -c "$(curl --fail --show-error --silent \
+#    --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # https://github.com/tonsky/FiraCode/wiki/Installing
 brew install --cask font-fira-code
 
 # https://www.kcl-lang.io/docs/user_docs/getting-started/install#homebrew-macos-1
-brew install kcl-lang/tap/kcl-lsp
+#brew install kcl-lang/tap/kcl-lsp
 
 # https://www.jetify.com/devbox/docs/installing_devbox/
 curl -fsSL https://get.jetify.com/devbox | bash
-
-# https://obsproject.com/kb/mac-installation#homebrew
-brew install --cask obs
 
 # https://github.com/hidetatz/kubecolor
 brew install kubecolor
